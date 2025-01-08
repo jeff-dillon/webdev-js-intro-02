5afd9041491e9d646e0c6592badea06caee82060
jeff-dillon <dillonjg@gmail.com>
2025-01-08 08:04:17 -0600

    completed assignment

--- js/assignment.js
+++ js/assignment.js
@@ -8,23 +8,33 @@ const willMeetResolutionElement = document.getElementById("will-meet-resolution"
 const submissionBtn = document.getElementById("submission-btn")
 
 // Declare your variables here.
+const resolution = "Get Better";
+const year = "2025";
+let willMeetResolution = true;
 
 function updateYear() {
     // write the logic
+    yearElement.textContent = year;
 
 }
 
 function updateResolution() {
     // Update this function 
+    resolutionElement.textContent = resolution;
+}
 
+function updateWillMeet() {
+    willMeetResolutionElement.textContent = willMeetResolution;
 }
 
 function render() {
     // Finish writing this function
-
+    updateYear();
+    updateResolution();
+    updateWillMeet();
 }
 
 submissionBtn.addEventListener("click", function () {
     // Finish writing this function
-
+    render()
 })
