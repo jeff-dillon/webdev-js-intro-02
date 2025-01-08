577c802d53b5b47e988275368159e4470ddec00f
jeff-dillon <dillonjg@gmail.com>
2025-01-08 07:52:28 -0600

    Completed part 1

--- js/index.js
+++ js/index.js
@@ -2,3 +2,52 @@
 // that are not declared
 "use strict"
 
+
+/*
+
+Primitive Data Types
+
+String: Represents a sequence of characters enclosed in single or double quotes.
+Number: Represents both integers and floating-point numbers.
+Boolean: Represents a logical value, either true or false.
+Undefined: Represents a variable that has been declared but not assigned a value.
+Null: Represents the intentional absence of any object value.
+Symbol: Represents a unique and immutable value, often used as keys for object properties.
+BigInt: Represents integers with arbitrary precision (introduced in ES2020)
+
+*/
+
+/*
+
+const myName = "Jeff";
+console.log(myName);
+console.log(typeof myName);
+
+const two = 2;
+console.log(two);
+console.log(typeof two);
+
+let isMentor = true;
+console.log(isMentor);
+console.log(typeof isMentor);
+
+isMentor = false;
+console.log(isMentor);
+console.log(typeof isMentor);
+
+let empty;
+console.log(empty);
+console.log(typeof empty);
+
+const myObj = {};
+console.log(myObj);
+console.log(typeof myObj);
+
+console.log(myObj.bubba);
+console.log(typeof myObj.bubba);
+
+const x = BigInt(Number.MAX_SAFE_INTEGER);
+console.log(x);
+console.log(typeof x);
+
+*/
